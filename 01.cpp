2057cc4c0e2591dd1b35e10e75f42cdcee5e3338
Douglas Liebl <112643737+DouglasLiebl@users.noreply.github.com>
2022-11-04 11:15:41 -0300

    Add files via upload

---
+++ 01.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    //Dado o tamanho da base e da altura de um retângulo, calcular a sua área e o seu perímetro.
+
+    int altura=10;
+    int base=5;
+    int area;
+    int perimetro;
+
+    area=altura*base;
+    perimetro=(altura*2)+(base*2);
+
+    cout << "Determinado retangulo possui as seguintes medidas: " << "\n";
+    cout << "Altura: " << altura << " metros \n" << "Base: " << base << " metros \n";
+    cout << "Sua area e perimetro sao, respectivamente: \n" << area << " metros quadrados \n" << perimetro << " metros \n";
+    return 0;
+}
