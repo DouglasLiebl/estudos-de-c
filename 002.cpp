67831ed04ae99ede4094259f0a2d591f8888367e
Douglas Liebl <112643737+DouglasLiebl@users.noreply.github.com>
2022-11-04 11:16:30 -0300

    Add files via upload

---
+++ 002.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+#define pi 3.1415
+#define canal cout << "Canal de Veneza\n\n";
+//VALORES OU COMANDOS CONSTANTES (QUE POSSUEM UM VALOR OU COMANDO FIXO) PODEM SER DEFINIDOS UTILIZANDO: #define
+
+int main(){
+    //AS VARIAVEIS PODEM SER DECLARADAS TODAS EM UMA UNICA LINHA DE CODIGO, COMO ESTA ABAIXO
+    int vidas=3, tiros=500, life=100; 
+
+    cout << pi << "\n\n";
+    canal
+
+
+    return 0;
+}
