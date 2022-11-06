3617863192d6685b21b1d3ac8de384b608a542b3
Douglas Liebl <112643737+DouglasLiebl@users.noreply.github.com>
2022-11-06 19:59:32 -0300

    Update 07.cpp

--- 07.cpp
+++ 07.cpp
@@ -11,12 +11,12 @@ int main(){
     cout << "Insira sua idade em dias: " << "\n";
     cin >> idade;
 
-    int anos=idade/365;
-    int res1=idade%365;
-    int meses=res1/30;
-    int dias=res1%30;
-
-    cout << "Se a sua idade e " << idade << " entao voce tem " << anos << " anos, " << meses << " meses e " <<dias << " dias de idade \n";
+        int anos=idade/365;
+        int res1=idade%365;
+        int meses=res1/30;
+        int dias=res1%30;
+    
+        cout << "Se a sua idade e " << idade << " entao voce tem " << anos << " anos, " << meses << " meses e " <<dias << " dias de idade \n";
 
     return 0;
 }
