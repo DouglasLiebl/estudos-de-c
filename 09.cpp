cf833e45c6ae0c8adb313742aaa9078692ac99cf
Douglas Liebl <112643737+DouglasLiebl@users.noreply.github.com>
2022-11-07 06:19:31 -0300

    Add files via upload

---
+++ 09.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+#define pi 3.14
+
+int main(){
+
+    //Faça um algoritmo que calcule e apresente o valor do volume de uma lata de óleo, dado seu raio e sua altura.
+
+    double raio;
+    double altura;
+
+    cout << "Insira o valor do raio de um cilindro: \n";
+    cin >> raio;
+    cout << "Agora insira o valor da altura: \n";
+    cin >> altura;
+
+        double volume=pi*((raio*raio)*altura);
+        
+        cout << "O volume dos valores fornecidos e: " << volume << " cm3 \n";
+
+    return 0;
+}
