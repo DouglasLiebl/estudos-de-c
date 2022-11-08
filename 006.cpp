de4c8a78330a20aa1b28ec0aabe16d0ece89ca65
Douglas Liebl <112643737+DouglasLiebl@users.noreply.github.com>
2022-11-08 06:16:26 -0300

    Add files via upload

---
+++ 006.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int num=15;
+    char opc='s';
+
+    if(num==10){ 
+        cout << "Valor de num igual a 10 \n";               
+    }else{
+        cout << "Valor de num maior que 10 \n";
+    }
+
+    //Comparadores logicos: > | < | >= | <= | == | !=
+    /* se (dia de sol){
+            vou ao clube
+       se nao
+            vou ao cinema     
+    }*/
+
+
+    return 0;
+}
